5a697cefc9dcd41faa6ecbe995fb43a1af863038
PatrickWalter214 <patrick.walter214@gmail.com>
2026-02-02 19:03:10 +0100

    Initialize Spring Boot app
    Initial Spring Boot app created with Spring Initializr with the
    following settings:
    - Project: Gradle - Groovy
    - Language: Java
    - Spring Boot: 4.0.2
    - Packaging: Jar
    - Configuration: YAML
    - Java: 25
    - Dependencies:
      - Spring Web
      - Spring Data JPA

---
+++ src/main/java/patrickwalter214/springboot/api/playground/SpringBootApiPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package patrickwalter214.springboot.api.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/patrickwalter214/springboot/api/playground/SpringBootApiPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package patrickwalter214.springboot.api.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
